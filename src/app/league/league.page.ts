ee88fd58c3ff8134922b96e1af13166d76d8c0f8
PC-22 <shahbaziweb@gmail.com>
2022-11-01 13:51:01 +0330

    init tournament

--- src/app/league/league.page.ts
+++ src/app/league/league.page.ts
@@ -50,7 +50,9 @@ export class LeaguePage {
       const draw = playerMatches.filter(isDraw).length;
       const lose = playerMatches.filter(isLoser).length;
       const gf = playerMatches.map(m => isWinner(m) ? m.gf : m.ga).reduce((a, b) => a + b, 0);
-      const ga = playerMatches.map(m => isLoser(m) ? m.ga : m.gf).reduce((a, b) => a + b, 0);
+      console.log(p.name, playerMatches.map(m => isWinner(m) ? m.gf : m.ga))
+      console.log(p.name, playerMatches.map(m => isLoser(m) ? m.ga : m.gf))
+      const ga = playerMatches.map(m => isLoser(m) ? m.gf : m.ga).reduce((a, b) => a + b, 0);
       const gd = (gf - ga) > 0 ? `+${gf - ga}` : (gf - ga).toString();
       const point = (win * 3) + draw;
       this.ranks.push({
